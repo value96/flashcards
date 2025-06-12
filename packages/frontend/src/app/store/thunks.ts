82eca7333adaade96437db6eb366944d297b7db7
v <v@email.com>
2025-06-12 04:40:38 +0300

    [frontend] fix wordsCounter

--- packages/frontend/src/app/store/thunks.ts
+++ packages/frontend/src/app/store/thunks.ts
@@ -1,12 +1,10 @@
 import { userModel } from '@entities/User'
-import { wordsCounterModel } from '@features/words-counter'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 export const initializeApp = createAsyncThunk(
   'App/initialize',
   async (_, { dispatch }) => {
     dispatch(checkRefreshTokenExpiration())
-    dispatch(wordsCounterModel.thunks.getCountWordsForUntilTomorrow())
   },
 )
 
--- packages/frontend/src/features/words-counter/ui/index.tsx
+++ packages/frontend/src/features/words-counter/ui/index.tsx
@@ -1,12 +1,20 @@
-import { useAppSelector } from '@shared/store'
+import { useAppDispatch, useAppSelector } from '@shared/store'
 import { wordsCounterModel } from '..'
 import styles from './styles.module.scss'
+import { useEffect } from 'react'
+import { thunks } from '../model'
 
 const { repeatedWordsWithinSession, countWordsToRepeatToday } =
   wordsCounterModel.selectors
 export const WordsCounter = () => {
+  const dispatch = useAppDispatch()
   const repeatedWords = useAppSelector(repeatedWordsWithinSession)
   const restCountWordsForToday = useAppSelector(countWordsToRepeatToday)
+
+  useEffect(() => {
+    if (restCountWordsForToday === null)
+      dispatch(thunks.getCountWordsForUntilTomorrow())
+  }, [restCountWordsForToday])
 
   return (
     <div className={styles.wordsCounter}>
